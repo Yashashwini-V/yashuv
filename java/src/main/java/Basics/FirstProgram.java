b1eb1d4b0604c713eddd0231b92477cb98da33a2
yashashwini <yashashwini@192.168.1.41>
2024-11-24 13:18:04 +0530

    Changing FristProgram

--- java/src/main/java/Basics/FirstProgram.java
+++ java/src/main/java/Basics/FirstProgram.java
@@ -2,7 +2,7 @@ package Basics;
 
 public class FirstProgram {
 	public static void main(String[] args) {
-	System.out.println("Hello World!!") ;
+	System.out.println("Hello Yashashwini!!") ;
 
 }
 }
