b752b1aee13270a396d2383ea044dfbfc95ff6d3
yashashwini <yashashwini@192.168.1.35>
2024-12-07 10:53:03 +0530

    brachingstatement.

---
+++ java/src/main/java/Basics/brachingstatement.java
@@ -0,0 +1,32 @@
+package Basics;
+
+public class brachingstatement {
+
+	public static void main(String[] args) {
+	/*	int row=1;
+		while(true) {
+			System.out.println(row);
+			if(row == 5) {
+
+				break;
+			}
+			
+			row++;
+		}
+		System.out.println("Outside of the loop");
+		
+		*/
+		
+		for(int row=1; row>=0; row++) {
+			System.out.println(row);
+			if(row == 6) {
+
+				break;
+			}
+			
+		}
+		System.out.println("Outside of the loop");
+	
+	}}
+
+
